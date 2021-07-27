6d28b2027fbba72599452500716134519f528162
rabi320 <ryonatan6@gmail.com>
2021-07-27 20:57:47 +0300

    App

--- Diamondapp.py
+++ Diamondapp.py
@@ -27,6 +27,54 @@ Continuous = X.describe().columns.tolist()
 
 #outlier detector
 
+
+# this is the main function in which we define our webpage 
+
+# giving the webpage a title
+st.title("Diamond Price Estimator 💎")
+image = Image.open('diamond.jpg')
+st.image(image)
+st.write('---')
+
+# here we define some of the front end elements of the web page like 
+# the font and background color, the padding and the text to be displayed
+html_temp = """
+<div style ="background-color:cyan;padding:13px">
+<h1 style ="color:black;text-align:center;font-family:Comic Sans MS;">How much is your diamond worth?</h1>
+</div>
+"""
+
+# this line allows us to display the front end aspects we have 
+# defined in the above code
+st.markdown(html_temp, unsafe_allow_html = True)
+
+# the following lines create text boxes in which the user can enter 
+# the data required to make the prediction
+
+def user_input_features():
+    carat = st.slider('Carat', 0.2, 5.01)
+    cut = st.selectbox("Quality of the cut", options = ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
+    color = st.selectbox("Diamond color, from J (worst) to D (best)", options = ['J', 'I', 'H', 'G', 'F', 'E','D'])
+    clarity = st.selectbox("Clarity of the diamond (I1 (worst), SI2, SI1, VS2, VS1, VVS2, VVS1, IF (best))", options = ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1",'IF'])
+    x = st.slider('Length', 0.01, 10.74)
+    y = st.slider('Width', 0.01, 58.9)
+    z = st.slider('Depth', 0.01, 31.8)
+    depth = st.slider('total depth (%)', 43, 95,step = 1)
+    table = st.slider('Width of top of diamond relative to widest point (%)', 43, 95,step = 1)    
+    data = {'carat': carat,
+            'cut': cut,
+            'color': color,
+            'clarity': clarity,
+            'depth': depth,
+            'table': table,
+            'x': x,
+            'y': y,
+            'z': z}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
 def Outlier_Detector(X,factor):
     X = pd.DataFrame(X).copy()
     for i in range(X.shape[1]):
@@ -65,70 +113,21 @@ preprocessor = ColumnTransformer(
         ('clarities', clarity_enc, ["clarity"])
     ])
 
-regressor = ExtraTreesRegressor(n_estimators = 200, max_features = 5,  random_state=42)
+model = ExtraTreesRegressor(n_estimators = 200, max_features = 5,  random_state=42)
 
 
 # Bundle preprocessing and modeling code in a pipeline
-model = Pipeline(steps=[('preprocessor', preprocessor),
-                              ('model', regressor)
+reg = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('model', model)
                              ])
 
 
-model.fit(X,y)
-
-# this is the main function in which we define our webpage 
-
-# giving the webpage a title
-st.title("Diamond Price Estimator 💎")
-image = Image.open('diamond.jpg')
-st.image(image)
-st.write('---')
-
-# here we define some of the front end elements of the web page like 
-# the font and background color, the padding and the text to be displayed
-html_temp = """
-<div style ="background-color:cyan;padding:13px">
-<h1 style ="color:black;text-align:center;font-family:Comic Sans MS;">How much is your diamond worth?</h1>
-</div>
-"""
-
-# this line allows us to display the front end aspects we have 
-# defined in the above code
-st.markdown(html_temp, unsafe_allow_html = True)
-
-# the following lines create text boxes in which the user can enter 
-# the data required to make the prediction
-
-def user_input_features():
-    carat = st.slider('Carat', 0.2, 5.01)
-    cut = st.selectbox("Quality of the cut", options = ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-    color = st.selectbox("Diamond color, from J (worst) to D (best)", options = ['J', 'I', 'H', 'G', 'F', 'E','D'])
-    clarity = st.selectbox("Clarity of the diamond (I1 (worst), SI2, SI1, VS2, VS1, VVS2, VVS1, IF (best))", options = ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1",'IF'])
-    x = st.slider('Length', 0.01, 10.74)
-    y = st.slider('Width', 0.01, 58.9)
-    z = st.slider('Depth', 0.01, 31.8)
-    depth = st.slider('total depth (%)', 43, 95,step = 1)
-    table = st.slider('Width of top of diamond relative to widest point (%)', 43, 95,step = 1)    
-
-    data = {'carat': carat,
-            'cut': cut,
-            'color': color,
-            'clarity': clarity,
-            'depth': round(depth,2),
-            'table': table,
-            'x': x,
-            'y': y,
-            'z': z}
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-df = user_input_features()
-
+reg.fit(X,y)
 
   
 # the below line ensures that when the button called 'Predict' is clicked, 
 # the prediction function defined above is called to make the prediction 
 # and store it in the variable result
 if st.button("Estimate Diamond Price"):
-    result = model.predict(df)[0]
+    result = reg.predict(df)[0]
     st.write(f'The Diamond is worth {result}$')
